27e1485707511f014f368fafc265ca21e10c9ef8
Tait Hoyem <tait@tait.tech>
2025-01-31 23:51:03 -0700

    more fmt

--- klatt/examples/make_sound.rs
+++ klatt/examples/make_sound.rs
@@ -1,48 +1,37 @@
-use rand::{
-	rngs::ThreadRng,
-	SeedableRng,
-};
-use klatt::{
-	generate_sound,
-	get_vocal_tract_transfer_function_coefficients,
-	m_parms,
-	f_params,
-};
-use hound::{
-	WavWriter,
-	WavSpec,
-	SampleFormat,
-};
+use hound::{SampleFormat, WavSpec, WavWriter};
+use klatt::{f_params, generate_sound, get_vocal_tract_transfer_function_coefficients, m_parms};
+use rand::{SeedableRng, rngs::ThreadRng};
 
 fn run_generate_sound() {
-		let rng = ThreadRng::default();
-		let mut wav = WavWriter::create("out.wav", WavSpec {
-				channels: 1,
-				sample_rate: 44100,
-				bits_per_sample: 32,
-				sample_format: SampleFormat::Float,
-		}).unwrap();
+    let rng = ThreadRng::default();
+    let mut wav = WavWriter::create(
+        "out.wav",
+        WavSpec {
+            channels: 1,
+            sample_rate: 44100,
+            bits_per_sample: 32,
+            sample_format: SampleFormat::Float,
+        },
+    )
+    .unwrap();
     let sound = generate_sound(&m_parms(), &vec![f_params()], rng);
     match sound {
         Ok(sound) => {
-					for sample in sound {
-            println!("{:?}", sample);
-						let s2: f32 = sample as f32;
-						wav.write_sample(s2).unwrap();
-					}
-				},
+            for sample in sound {
+                println!("{:?}", sample);
+                let s2: f32 = sample as f32;
+                wav.write_sample(s2).unwrap();
+            }
+        }
         Err(error) => {
             println!("Error: {}", error);
             std::process::exit(1);
-			}
+        }
     }
 }
 
 fn run_generate_vocal() {
-    let vocal = get_vocal_tract_transfer_function_coefficients(
-        &m_parms(),
-        &f_params(),
-    );
+    let vocal = get_vocal_tract_transfer_function_coefficients(&m_parms(), &f_params());
     match vocal {
         Ok(vocal) => println!("Vocal: {:#?}", &vocal),
         Err(vocal) => {
